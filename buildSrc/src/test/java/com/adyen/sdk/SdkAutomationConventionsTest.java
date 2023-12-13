8366e88373449dbb8064b6cff5ac00d34ea17fa5
michaelpa <michael.paul@adyen.com>
2023-12-13 17:22:58 +0100

    Extract Service class

--- buildSrc/src/test/java/com/adyen/sdk/SdkAutomationConventionsTest.java
+++ buildSrc/src/test/java/com/adyen/sdk/SdkAutomationConventionsTest.java
@@ -5,6 +5,7 @@ import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.Test;
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SdkAutomationConventionsTest {
@@ -14,5 +15,15 @@ public class SdkAutomationConventionsTest {
         project.getPluginManager().apply("adyen.sdk-automation-conventions");
 
         assertTrue(project.getTasks().getByName("generateCheckout") instanceof GenerateTask);
+        assertTrue(project.getExtensions().getExtraProperties().has("services"));
+    }
+
+    @Test
+    public void serviceName() {
+        var svc = new Service();
+        svc.setName("Checkout");
+        svc.setVersion(71);
+
+        assertEquals("CheckoutService-v71.json", svc.getFilename());
     }
 }
