7fcb0303037f8e30db8ab43813f80c932e72fd9b
beppe <beppe.catanese@adyen.com>
2026-03-20 10:42:11 +0100

    Check System.getProperty first

--- buildSrc/src/main/kotlin/adyen.sdk-automation-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/adyen.sdk-automation-conventions.gradle.kts
@@ -12,8 +12,8 @@ val propsFile = rootProject.file("buildSrc/gradle.properties")
 if (propsFile.exists()) {
     propsFile.inputStream().use { props.load(it) }
 }
-val openApiVersion: String? = project.findProperty("openapiGeneratorVersion") as String?
-    ?: System.getProperty("openapiGeneratorVersion")
+val openApiVersion: String? = System.getProperty("openapiGeneratorVersion")
+    ?: project.findProperty("openapiGeneratorVersion") as String?
     ?: props.getProperty("openapiGeneratorVersion")
 
 repositories {
